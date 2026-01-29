02a1fbbaf73826d3e6cec4f837bee9b6ecc67be7
Asadullah Al-jayashi <69217567+Perchanxe@users.noreply.github.com>
2026-01-28 19:10:51 -0600

    Installed glob

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,24 @@
 import { defineConfig } from "vite";
-import { resolve, dirname, relative } from "path";
-import { fileURLToPath } from "url";
-import glob from "glob";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { resolve } from 'path';
+import { glob } from 'glob';
+import path from 'path';
 
 // Find all HTML files in the site directory
-const siteHtmlFiles = glob
-  .sync("site/**/*.html")
-  .map((file) => [
-    relative("site", file).replace(/\.html$/, ""), // key
-    resolve(__dirname, file),                     // absolute path
-  ]);
+const siteHtmlFiles = glob.sync('site/**/*.html').map(file => [
+    path.relative('site', file).replace(/\.html$/, ''),
+    resolve(__dirname, file)
+]);
 
 // Combine with root index.html
 const htmlFiles = Object.fromEntries([
-  ["index", resolve(__dirname, "index.html")],
-  ...siteHtmlFiles,
+    ['index', resolve(__dirname, 'index.html')], // Root index.html
+    ...siteHtmlFiles
 ]);
 
 export default defineConfig({
-  // remove tailwind plugin if you aren't using it
-  build: {
-    rollupOptions: {
-      input: htmlFiles,
-    },
-  },
+    build: {
+        rollupOptions: {
+            input: htmlFiles
+        }
+    }
 });
